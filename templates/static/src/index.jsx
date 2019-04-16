b389aac72c165bdbe8d08a6edd2f18150e04d003
clementine contat <clementine.contat@gmail.com>
2019-04-16 12:32:13 +0100

    simplified the front-end part, removing routes

--- templates/static/src/index.jsx
+++ templates/static/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import routes from "./routes";
+import App from './components/app'
 
-ReactDOM.render(routes, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
